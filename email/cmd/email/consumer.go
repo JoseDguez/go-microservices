9202e7832b8632fcac1c363091238f3cdb112026
Jose Antonio Dominguez <joseantonio.dguez@gmail.com>
2024-08-22 18:02:40 -0700

    refactor: integrate kafka consumer groups to prevent consume duplication

--- email/cmd/email/consumer.go
+++ email/cmd/email/consumer.go
@@ -1,76 +1,78 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/IBM/sarama"
 	"github.com/JoseDguez/go-microservices/internal/email"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
-const topic = "email"
+const (
+	topic   = "email"
+	groupID = "email-consumer-group"
+)
 
-var wg sync.WaitGroup
+type ConsumerGroupHandler struct{}
 
-type EmailMsg struct {
-	OrderId string `json:"order_id"`
-	UserID  string `json:"user_id"`
+func (ConsumerGroupHandler) Setup(_ sarama.ConsumerGroupSession) error   { return nil }
+func (ConsumerGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
+
+func (ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		log.Printf("Partition %d - Received message: %s\n", message.Partition, string(message.Value))
+		handleMessage(message)
+		session.MarkMessage(message, "")
+	}
+	return nil
 }
 
 func main() {
 	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
-	done := make(chan struct{})
 
-	consumer, err := sarama.NewConsumer([]string{"my-cluster-kafka-bootstrap:9092"}, sarama.NewConfig())
+	saramaConfig := sarama.NewConfig()
+	saramaConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
+	consumerGroup, err := sarama.NewConsumerGroup([]string{"my-cluster-kafka-bootstrap:9092"}, groupID, saramaConfig)
 	if err != nil {
-		log.Fatalf("Failed to start Sarama consumer: %v", err)
+		log.Fatalf("Failed to create sarama consumer group: %v", err)
 	}
+	defer func(consumerGroup sarama.ConsumerGroup) {
+		err := consumerGroup.Close()
+		if err != nil {
+			log.Println("Failed to close sarama consumer group:", err)
+		}
+	}(consumerGroup)
 
-	defer func() {
-		close(done)
-		if err := consumer.Close(); err != nil {
-			log.Println("Failed to close Sarama consumer:", err)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			if err := consumerGroup.Consume(ctx, []string{topic}, ConsumerGroupHandler{}); err != nil {
+				log.Fatalf("Error from consumer: %v", err)
+			}
+			// Check if context was cancelled, signaling that we should stop consuming
+			if ctx.Err() != nil {
+				return
+			}
 		}
 	}()
 
-	partitions, err := consumer.Partitions(topic)
-	if err != nil {
-		log.Fatalf("Failed to get partitions: %v", err)
-	}
-
-	for _, partition := range partitions {
-		partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
-		if err != nil {
-			log.Fatalf("Failed to start consumer for partition %d: %v", partition, err)
-		}
-
-		defer func() {
-			if err := partitionConsumer.Close(); err != nil {
-				log.Println("Failed to close Sarama consumer:", err)
-			}
-		}()
-
-		wg.Add(1)
-		go awaitMessages(partitionConsumer, partition, done)
-	}
-
+	// Handle termination signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Received shutdown signal")
+	cancel()
 	wg.Wait()
-}
-
-func awaitMessages(pc sarama.PartitionConsumer, partition int32, done chan struct{}) {
-	defer wg.Done()
-
-	for {
-		select {
-		case msg := <-pc.Messages():
-			log.Printf("Partition %d - Received message: %s\n", partition, string(msg.Value))
-			handleMessage(msg)
-		case <-done:
-			log.Printf("Received done signal. Exiting partition %d\n", partition)
-			return
-		}
-	}
 }
 
 func handleMessage(msg *sarama.ConsumerMessage) {
